c1c01baf609f324ad9a3705a6fc0ebf70cbf2833
francesco <histats@gmail.com>
2015-03-23 10:52:45 +0000

    now check shell env - fixed

--- src/ipth-executor.sh
+++ src/ipth-executor.sh
@@ -24,7 +24,7 @@ config[template_file]=$2
 IPTH_ACTION=${3:-enable}
 
 
-[ "${script[name]}" != "bash" ] && {
+[ "$BASH" != "bash" ] && {
     echo 'script is only bash compatible at the moment > '"${script[name]}"
     exit 1
 }
